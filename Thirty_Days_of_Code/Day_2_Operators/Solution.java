a78b2acc42bd4faee95cfb0cbce550e4621d52e3
dideles <didenkoelenas@yandex.ru>
2024-06-18 12:08:21 +0700

    Merge remote-tracking branch 'origin/main'

--- Thirty_Days_of_Code/Day_2_Operators/Solution.java
+++ Thirty_Days_of_Code/Day_2_Operators/Solution.java
@@ -1,38 +1,8 @@
 package Thirty_Days_of_Code.Day_2_Operators;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
-class Result {
-
-    /*
-     * Complete the 'solve' function below.
-     *
-     * The function accepts following parameters:
-     *  1. DOUBLE meal_cost
-     *  2. INTEGER tip_percent
-     *  3. INTEGER tax_percent
-     */
-
-    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        // Write your code here
-        double tip=meal_cost*tip_percent/100;
-        double tax=meal_cost*tax_percent/100;
-        int totalCost=(int)Math.round(meal_cost+tip+tax);
-        System.out.print(totalCost);
-
-    }
-
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
